84371cd77947062b5fd1305e18a0ef35e956707a
Qiuyun <97208294@qq.com>
2026-02-13 10:43:03 +0800

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import sitemap from '@astrojs/sitemap';
-import { siteConfig } from '@/configs/site-config';
+import { siteConfig } from './src/configs/site-config';
 
 // https://astro.build/config
 export default defineConfig({
